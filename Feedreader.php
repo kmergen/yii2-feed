0e33d515a0d995f27e2ff20f004649c4bd097c6b
kmergen <klaus.mergen@web.de>
2014-12-06 18:30:51 +0100

    change Feedreader.php

--- Feedreader.php
+++ Feedreader.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
  * @copyright Copyright &copy; Klaus Mergen, 2014
  * @package yii2-widgets
@@ -10,26 +9,53 @@ namespace kmergen\feadreader;
 
 use yii\helpers\Html;
 
-
-
 /**
- * Feedreader provides an RSS and Atom Reader.
+ * Feedreader widget provides an RSS and Atom Reader.
  *
  * @author Klaus Mergen <klaus.mergen@web.de>
  * @since 1.0
  */
-
 class Feedreader extends yii\widgets\Widget
 {
-    
-      
+
+    public $feedUrl;
+    protected $feed;
+
     public function init()
     {
-     
+        //At the moment we can only use http protocol
+        $this->feedUrl = str_replace('https', 'http', $this->feedUrl);
     }
 
     public function run()
     {
+        $this->feed = Yii::$app->feed->reader()->import($this->feedUrl);
+
+        $data = array(
+            'title' => $feed->getTitle(),
+            'link' => $feed->getLink(),
+            'dateModified' => $feed->getDateModified(),
+            'description' => $feed->getDescription(),
+            'language' => $feed->getLanguage(),
+            'entries' => array(),
+        );
+
+        foreach ($feed as $entry) {
+            $edata = array(
+                'title' => $entry->getTitle(),
+                'description' => $entry->getDescription(),
+                'dateModified' => $entry->getDateModified(),
+                'authors' => $entry->getAuthors(),
+                'link' => $entry->getLink(),
+                'content' => $entry->getContent()
+            );
+            $data['entries'][] = $edata;
+        }
+        
+        foreach($data['entries'] as $entry) {
+            echo '<h3>' . Html::encode($entry['title']) . '</h3>';
+            echo Html::encode($entry['description']);
+        }
 
         $this->registerClientScript();
     }
@@ -43,7 +69,7 @@ class Feedreader extends yii\widgets\Widget
         $js = <<<JS
                
 JS;
-       
+
         $this->view->registerJs($js);
     }
 
