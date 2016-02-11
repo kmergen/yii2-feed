eeea19b0257bacaabf2198d6e0887d1ea8333e9a
kmergen <klaus.mergen@web.de>
2016-02-11 18:21:25 +0100

    changes configuration

--- Feedreader.php
+++ Feedreader.php
@@ -13,7 +13,7 @@ use Zend\Feed\Reader\Reader;
 /**
  * Feedreader widget provides an RSS and Atom Reader.
  *
- * @author Klaus Mergen <klaus.mergen@web.de>
+ * @author Klaus Mergen <kmergenweb@gmail.com>
  * @since 1.0
  */
 class Feedreader extends \yii\base\Widget
@@ -28,11 +28,20 @@ class Feedreader extends \yii\base\Widget
      * @var string the template file. This is the view that is rendered by the widget to show the feeds.
      */
     public $templateFile = 'default';
+    
+    /**
+     * @var string the css class for the feed. If null it will be set in [[init()]]
+     */
+    public $wrapperClass;
         
     public function init()
     {
         //At the moment we can only use http protocol
         $this->feedUrl = str_replace('https', 'http', $this->feedUrl);
+        
+        if ($this->wrapperClass === null) {
+            $this->wrapperClass = 'feed-' . $this->templateFile;
+        }
     }
 
     public function run()
